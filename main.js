d186ae7bd34a73d2f955b332d9483b6fbf4f08eb
fireteam99 <raymancheungsy@gmail.com>
2020-12-31 21:34:27 -0500

    set up babel so that tests can be run using esm

---
+++ main.js
@@ -0,0 +1,19 @@
+import getSuccessors, {
+    isValidX,
+    isValidY,
+    isRailroad,
+    isValidDestination,
+    placePiece,
+    generateAdjList,
+    isCamp,
+  } from "./getSuccessors";
+
+export { getSuccessors, 
+    isValidX, 
+    isValidY, 
+    isRailroad,
+    isValidDestination,
+    placePiece,
+    generateAdjList,
+    isCamp,
+};
