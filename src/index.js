0f8266b9e429495c5c34de48fc694841e45be31d
fireteam99 <raymancheungsy@gmail.com>
2021-01-14 16:18:31 -0500

    fixed exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-export const { isCamp, isHQ, iterBoard, mapBoard } = require('./core');
-export const {
-    isValidX,
-    isValidY,
-    isValidDestination,
-    isRailroad,
-    getSuccessors,
-    generateAdjList,
-    placePiece,
-} = require('./getSuccessors');
-export const { pieces, Piece } = require('./piece.js');
-export const validateSetup = require('./validateSetup');
+export * from './core';
+export { default } from './getSuccessors';
+export * from './getSuccessors';
+export * from './piece.js';
+export { default } from './validateSetup';
