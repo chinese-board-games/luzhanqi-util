f1a8b9bac7b06743b55ecf3e0fddb8145ec0b9a5
fireteam99 <raymancheungsy@gmail.com>
2021-01-13 19:59:06 -0500

    added semicolons to prettier config and replaced esm with common.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const core = require('./src/core');
+const getSuccessors = require('./src/getSuccessors');
+const piece = require('./src/piece.js');
+const validateSetup = require('./src/validateSetup');
 module.exports = {
     ...core,
+    ...getSuccessors,
+    ...piece,
+    validateSetup,
 };
